57921090e197e852160479b66028ffa0fb1cf76b
samlimby <129619263+samlimby@users.noreply.github.com>
2023-04-01 19:05:57 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+const digitDisplay = document.getElementById('digit-display');
+const ctaButton = document.getElementById('cta-display');
+const secondaryButton = document.getElementById('secondary-display');
+
+let timer = null;
+let startTime = null;
+
+ctaButton.addEventListener('click', () => {
+  if (timer !== null) {
+    return;
+  }
+  startTime = Date.now();
+  timer = setInterval(updateTimer, 10);
+});
+
+secondaryButton.addEventListener('click', () => {
+  clearInterval(timer);
+  timer = null;
+  digitDisplay.textContent = '00:00';
+});
+
+function updateTimer() {
+  const elapsedTime = Date.now() - startTime;
+  const minutes = Math.floor(elapsedTime / 60000);
+  const seconds = Math.floor((elapsedTime % 60000) / 1000);
+  const milliseconds = Math.floor((elapsedTime % 1000) / 10);
+  digitDisplay.textContent = `${padTime(minutes)}:${padTime(seconds)}`;
+}
+
+function padTime(time) {
+  return time.toString().padStart(2, '0');
+}
